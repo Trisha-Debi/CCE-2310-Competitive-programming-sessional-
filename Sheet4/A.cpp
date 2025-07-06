112d30831f1cc8ba1cb6a881a9e56d777bb527cd
Trisha-Debi <nath92482@gmail.com>
2025-07-07 00:34:42 +0600

    Update A.cpp

--- Sheet4/A.cpp
+++ Sheet4/A.cpp
@@ -1,1 +1,11 @@
-
+//Course Title :Competitive programming Sessional
+//Course Code :CCE-2310
+//Mirza raquib
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+string s,t;
+cin>>s>>t;
+cout<<s.size()<<" "<<t.size()<<endl;
+cout<<s<<" "<<t<<endl;
+}
