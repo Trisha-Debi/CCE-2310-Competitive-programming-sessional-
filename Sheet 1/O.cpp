1748c2c5bca939e0e9527b05e71ba358122ee60d
Trisha-Debi <nath92482@gmail.com>
2025-07-12 00:14:41 +0600

    Update O.cpp

--- Sheet 1/O.cpp
+++ Sheet 1/O.cpp
@@ -1,1 +1,35 @@
+1.// Problem name: O. Calculator 
+2.//Course title: Competitive Programming Sessional 
+3.//Course code: 2310
+4.// Course teacher name: Mirza Raquib
+5.//Date: 23/4/25
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int A, B;
+    char S;
 
+    cin>> A >> S >> B;
+
+    switch(S) {
+        case '+':
+            cout << A + B <<endl;
+            break;
+        case '-':
+            cout << A - B <<endl;
+            break;
+        case '*':
+            cout << A * B <<endl;
+            break;
+        case '/':
+            if (B != 0) {
+                cout<< A / B <<endl;
+            } else {
+                cout << "Division by zero is undefined." <<endl;
+            }
+            break;
+        default:
+            cout << "Invalid operator. Use one of +, -, *, /." <<endl;
+    }
+
+}
