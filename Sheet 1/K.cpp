cd6714f45afac03191d681faf6fb3f3107ebe211
Trisha-Debi <nath92482@gmail.com>
2025-07-12 00:11:53 +0600

    Update K.cpp

--- Sheet 1/K.cpp
+++ Sheet 1/K.cpp
@@ -1,1 +1,16 @@
+1.// Problem name: K. Max and Min  
+2.//Course title: Competitive Programming Sessional 
+3.//Course code: 2310
+4.// Course teacher name: Mirza Raquib
+5.//Date: 23/4/25
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    long long a,b,c,minimum,maximum;
+    cin>>a>>b>>c;
+    minimum= min({a,b,c});
+    maximum= max({a,b,c});
+    cout<<minimum<<" "<<maximum<<endl;
+}
