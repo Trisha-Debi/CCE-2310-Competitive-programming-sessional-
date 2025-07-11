72794d61be736649eee6a2a4167f5aeec7e1fecc
Trisha-Debi <nath92482@gmail.com>
2025-07-12 00:37:24 +0600

    Update U.cpp

--- sheet2/U.cpp
+++ sheet2/U.cpp
@@ -1,1 +1,31 @@
 
+1.// Problem name: U. Some Sums  
+2.//Course title: Competitive Programming Sessional 
+3.//Course code: 2310
+4.// Course teacher name: Mirza Raquib
+5.//Date: 23/4/25
+#include<bits/stdc++.h>
+using namespace std;
+int digitSum(int n) {
+    int sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int main() {
+    int N, A, B;
+    cin >> N >> A >> B;
+
+    int total = 0;
+    for (int i = 1; i <= N; ++i) {
+        int sum = digitSum(i);
+        if (sum >= A && sum <= B) {
+            total += i;
+        }
+    }
+
+    cout << total << endl;
+}
