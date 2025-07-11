ad7dba19361e917fec20abc1b4e270e9359b4727
Trisha-Debi <nath92482@gmail.com>
2025-07-12 00:29:58 +0600

    Update U.cpp

--- Sheet 1/U.cpp
+++ Sheet 1/U.cpp
@@ -1,1 +1,22 @@
+1.// Problem name: U. Float or int  
+2.//Course title: Competitive Programming Sessional 
+3.//Course code: 2310
+4.// Course teacher name: Mirza Raquib
+5.//Date: 23/4/25
+#include<bits/stdc++.h>
+using namespace std;
 
+int main() {
+    float N;
+    cin >> N;
+
+    int intPart = (int)N;
+    float decimalPart = N - intPart;
+
+    if (decimalPart == 0) {
+        cout << "int " << intPart << endl;
+    }
+    else {
+        cout << "float " << intPart << " " << fixed << setprecision(3) << decimalPart << endl;
+    }
+}
