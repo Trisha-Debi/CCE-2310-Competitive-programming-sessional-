54c2bf67ee63736c4011e19f8485e9d97186c141
Trisha-Debi <nath92482@gmail.com>
2025-07-12 00:30:50 +0600

    Update T.cpp

--- Sheet 1/T.cpp
+++ Sheet 1/T.cpp
@@ -1,1 +1,22 @@
 
+1.// Problem name: T. Sort Numbers  
+2.//Course title: Competitive Programming Sessional 
+3.//Course code: 2310
+4.// Course teacher name: Mirza Raquib
+5.//Date: 23/4/25
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+    vector<int> nums = {A, B, C};
+    sort(nums.begin(), nums.end());
+    for (int num : nums) {
+        cout << num << endl;
+    }
+    cout << endl;
+    cout << A << endl;
+    cout << B << endl;
+    cout << C << endl;
+}
