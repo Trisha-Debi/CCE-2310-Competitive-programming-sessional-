2f30f50fe4043b3c96dd1f6217f2316e867eaa20
Trisha-Debi <nath92482@gmail.com>
2025-08-02 01:09:37 +0600

    Update and rename F to F.cpp

---
+++ sheet2/F.cpp
@@ -0,0 +1,18 @@
+//Course title:Competitive programming sessional
+//Course code:CCE-2310
+//Course teacher:Mirza Raquib
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    long long n;
+
+    cin>>n;
+    for(int i=1;i<13;i++)
+    {
+
+      cout<<n<<" * "<< i <<" = "<<n*i<<endl;
+
+    }
+}
