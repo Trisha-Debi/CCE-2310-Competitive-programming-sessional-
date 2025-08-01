b2584162cfde7349fbdf275b1f33dea5d384bb2b
Trisha-Debi <nath92482@gmail.com>
2025-08-02 01:20:30 +0600

    Update and rename X to X.cpp

---
+++ sheet2/X.cpp
@@ -0,0 +1,25 @@
+//Course title:Competitive programming sessional
+//Course code:CCE-2310
+//Course teacher:Mirza Raquib
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        long long n,one=0,r;
+        cin>>n;
+        while(n!=0)
+        {
+            r=n%2;
+            if(r==1)one++;
+            n=n/2;
+    }
+    long long ans=pow(2,one);
+    cout<<ans-1<<endl;
+
+    }
+
+}
